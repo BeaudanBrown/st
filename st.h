c71869d57d97070de2c70b2dc36ed8d336c4e13c
Beaudan Brown <beaudan.brown@gmail.com>
2021-03-01 14:50:10 +1100

    Remove codepoint shortcut

--- st.h
+++ st.h
@@ -89,7 +89,6 @@ void redraw(void);
 void draw(void);
 
 void externalpipe(const Arg *);
-void iso14755(const Arg *);
 void kscrolldown(const Arg *);
 void kscrollup(const Arg *);
 void printscreen(const Arg *);
